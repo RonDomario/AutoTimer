7abddd06c844d04fb392892011e4350079d6ebf1
RonDomario <zharkovroman1998@gmail.com>
2025-11-19 12:25:31 +0400

    Renamed the knobs. Some visual changes. Making the settings window.

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ class Window(QtWidgets.QMainWindow):
         super().__init__()
         self.position = None
         self.always_on_top = True
-        self.duration = None
-        self.frequency = None
+        self.break_duration = None
+        self.work_duration = None
         self.window_width = 200
         self.window_height = 300
         self.phase = 1
@@ -45,7 +45,8 @@ class Window(QtWidgets.QMainWindow):
         self.always_on_top_checkbox.stateChanged.connect(self.changeAlwaysOnTop)
         self.top_layout.addWidget(self.always_on_top_checkbox)
         self.info_button = QtWidgets.QPushButton("Info")
-        self.info_button.clicked.connect(self.showInfo)
+        self.info_button.clicked.connect(self.openSettings)
+        # self.info_button.clicked.connect(self.showInfo)
         self.top_layout.addWidget(self.info_button)
 
         self.opacity_label = QtWidgets.QLabel("Opacity: 100%")
@@ -57,57 +58,57 @@ class Window(QtWidgets.QMainWindow):
         self.opacity_slider.valueChanged.connect(self.changeOpacity)
         self.layout.addWidget(self.opacity_slider)
 
-        self.frequency_label = QtWidgets.QLabel("Frequency: 0h 0m 0s")
-        self.layout.addWidget(self.frequency_label)
-        self.frequency_layout = QtWidgets.QHBoxLayout()
-        self.layout.addLayout(self.frequency_layout)
+        self.work_label = QtWidgets.QLabel("Work: 0h 0m 0s")
+        self.layout.addWidget(self.work_label)
+        self.work_layout = QtWidgets.QHBoxLayout()
+        self.layout.addLayout(self.work_layout)
 
-        self.frequency_hours = QtWidgets.QDial()
-        self.frequency_hours.setRange(0, 59)
-        self.frequency_hours.setWrapping(False)
-        self.frequency_hours.setNotchesVisible(True)
-        self.frequency_hours.valueChanged.connect(self.changeFrequency)
-        self.frequency_layout.addWidget(self.frequency_hours)
+        self.work_hours = QtWidgets.QDial()
+        self.work_hours.setRange(0, 59)
+        self.work_hours.setWrapping(False)
+        self.work_hours.setNotchesVisible(True)
+        self.work_hours.valueChanged.connect(self.changeWorkDuration)
+        self.work_layout.addWidget(self.work_hours)
 
-        self.frequency_minutes = QtWidgets.QDial()
-        self.frequency_minutes.setRange(0, 59)
-        self.frequency_minutes.setWrapping(False)
-        self.frequency_minutes.setNotchesVisible(True)
-        self.frequency_minutes.valueChanged.connect(self.changeFrequency)
-        self.frequency_layout.addWidget(self.frequency_minutes)
+        self.work_minutes = QtWidgets.QDial()
+        self.work_minutes.setRange(0, 59)
+        self.work_minutes.setWrapping(False)
+        self.work_minutes.setNotchesVisible(True)
+        self.work_minutes.valueChanged.connect(self.changeWorkDuration)
+        self.work_layout.addWidget(self.work_minutes)
 
-        self.frequency_seconds = QtWidgets.QDial()
-        self.frequency_seconds.setRange(0, 59)
-        self.frequency_seconds.setWrapping(False)
-        self.frequency_seconds.setNotchesVisible(True)
-        self.frequency_seconds.valueChanged.connect(self.changeFrequency)
-        self.frequency_layout.addWidget(self.frequency_seconds)
+        self.work_seconds = QtWidgets.QDial()
+        self.work_seconds.setRange(0, 59)
+        self.work_seconds.setWrapping(False)
+        self.work_seconds.setNotchesVisible(True)
+        self.work_seconds.valueChanged.connect(self.changeWorkDuration)
+        self.work_layout.addWidget(self.work_seconds)
 
-        self.duration_label = QtWidgets.QLabel("Duration: 0h 0m 0s")
-        self.layout.addWidget(self.duration_label)
-        self.duration_layout = QtWidgets.QHBoxLayout()
-        self.layout.addLayout(self.duration_layout)
+        self.brake_label = QtWidgets.QLabel("Break: 0h 0m 0s")
+        self.layout.addWidget(self.brake_label)
+        self.break_layout = QtWidgets.QHBoxLayout()
+        self.layout.addLayout(self.break_layout)
 
-        self.duration_hours = QtWidgets.QDial()
-        self.duration_hours.setRange(0, 59)
-        self.duration_hours.setWrapping(False)
-        self.duration_hours.setNotchesVisible(True)
-        self.duration_hours.valueChanged.connect(self.changeDuration)
-        self.duration_layout.addWidget(self.duration_hours)
+        self.break_hours = QtWidgets.QDial()
+        self.break_hours.setRange(0, 59)
+        self.break_hours.setWrapping(False)
+        self.break_hours.setNotchesVisible(True)
+        self.break_hours.valueChanged.connect(self.changeBreakDuration)
+        self.break_layout.addWidget(self.break_hours)
 
-        self.duration_minutes = QtWidgets.QDial()
-        self.duration_minutes.setRange(0, 59)
-        self.duration_minutes.setWrapping(False)
-        self.duration_minutes.setNotchesVisible(True)
-        self.duration_minutes.valueChanged.connect(self.changeDuration)
-        self.duration_layout.addWidget(self.duration_minutes)
+        self.break_minutes = QtWidgets.QDial()
+        self.break_minutes.setRange(0, 59)
+        self.break_minutes.setWrapping(False)
+        self.break_minutes.setNotchesVisible(True)
+        self.break_minutes.valueChanged.connect(self.changeBreakDuration)
+        self.break_layout.addWidget(self.break_minutes)
 
-        self.duration_seconds = QtWidgets.QDial()
-        self.duration_seconds.setRange(0, 59)
-        self.duration_seconds.setWrapping(False)
-        self.duration_seconds.setNotchesVisible(True)
-        self.duration_seconds.valueChanged.connect(self.changeDuration)
-        self.duration_layout.addWidget(self.duration_seconds)
+        self.break_seconds = QtWidgets.QDial()
+        self.break_seconds.setRange(0, 59)
+        self.break_seconds.setWrapping(False)
+        self.break_seconds.setNotchesVisible(True)
+        self.break_seconds.valueChanged.connect(self.changeBreakDuration)
+        self.break_layout.addWidget(self.break_seconds)
 
         self.time_value = QtWidgets.QLCDNumber()
         self.time_value.setDigitCount(8)
@@ -143,6 +144,10 @@ class Window(QtWidgets.QMainWindow):
             self.success.setSource(QtCore.QUrl.fromLocalFile(resource_path("media/sounds/success.wav")))
             self.success.setVolume(0.2)
 
+    def openSettings(self):
+        settings = Settings(self)
+        settings.exec()
+
     def changeAlwaysOnTop(self):
         self.always_on_top = self.always_on_top_checkbox.isChecked()
         self.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint, self.always_on_top)
@@ -160,95 +165,125 @@ class Window(QtWidgets.QMainWindow):
         self.opacity_label.setText(f"Opacity: {value}%")
         self.setWindowOpacity(value / 100)
 
-    def changeFrequency(self, _):
-        hours = self.frequency_hours.value()
-        minutes = self.frequency_minutes.value()
-        seconds = self.frequency_seconds.value()
-        self.frequency_label.setText(f"Frequency: {hours}h {minutes}m {seconds}s")
+    def changeWorkDuration(self, _):
+        hours = self.work_hours.value()
+        minutes = self.work_minutes.value()
+        seconds = self.work_seconds.value()
+        self.work_label.setText(f"Frequency: {hours}h {minutes}m {seconds}s")
         self.time_value.display(f"{hours:02}:{minutes:02}:{seconds:02}")
 
-    def changeDuration(self, _):
-        hours = self.duration_hours.value()
-        minutes = self.duration_minutes.value()
-        seconds = self.duration_seconds.value()
-        self.duration_label.setText(f"Duration: {hours}h {minutes}m {seconds}s")
+    def changeBreakDuration(self, _):
+        hours = self.break_hours.value()
+        minutes = self.break_minutes.value()
+        seconds = self.break_seconds.value()
+        self.brake_label.setText(f"Duration: {hours}h {minutes}m {seconds}s")
+
+    @staticmethod
+    def timeToHMS(time: int):
+        hours, minutes = divmod(time, 3600)
+        minutes, seconds = divmod(minutes, 60)
+        return hours, minutes, seconds
 
     def updateTimer(self, value):
         value -= 1
-        hours, minutes = divmod(value, 3600)
-        minutes, seconds = divmod(minutes, 60)
+        hours, minutes, seconds = self.timeToHMS(value)
         self.time_value.display(f"{hours:02}:{minutes:02}:{seconds:02}")
         return value
 
     def toggleTime(self):
         if self.phase == 1:
-            self.frequency = self.updateTimer(self.frequency)
-            if self.frequency <= 0:
-                self.frequency = (self.frequency_hours.value() * 3600 +
-                                  self.frequency_minutes.value() * 60 +
-                                  self.frequency_seconds.value())
+            self.work_duration = self.updateTimer(self.work_duration)
+            if self.work_duration <= 0:
+                self.work_duration = (self.work_hours.value() * 3600 +
+                                      self.work_minutes.value() * 60 +
+                                      self.work_seconds.value())
                 self.phase = 2
                 self.alert.play()
         elif self.phase == 2:
-            self.duration = self.updateTimer(self.duration)
-            if self.duration <= 0:
-                self.duration = (self.duration_hours.value() * 3600 +
-                                 self.duration_minutes.value() * 60 +
-                                 self.duration_seconds.value())
+            self.break_duration = self.updateTimer(self.break_duration)
+            if self.break_duration <= 0:
+                self.break_duration = (self.break_hours.value() * 3600 +
+                                       self.break_minutes.value() * 60 +
+                                       self.break_seconds.value())
                 self.phase = 1
                 self.success.play()
 
     def startClock(self):
-        self.frequency = (self.frequency_hours.value() * 3600 +
-                          self.frequency_minutes.value() * 60 +
-                          self.frequency_seconds.value())
-        self.duration = (self.duration_hours.value() * 3600 +
-                         self.duration_minutes.value() * 60 +
-                         self.duration_seconds.value())
-        if self.frequency == 0 and self.duration == 0:
+        self.work_duration = (self.work_hours.value() * 3600 +
+                              self.work_minutes.value() * 60 +
+                              self.work_seconds.value())
+        self.break_duration = (self.break_hours.value() * 3600 +
+                               self.break_minutes.value() * 60 +
+                               self.break_seconds.value())
+        if self.work_duration == 0 and self.break_duration == 0:
             return
         self.phase = 1
         self.skip_button.setText("Skip\nBreak")
         self.start_button.setEnabled(False)
-        self.frequency_hours.setEnabled(False)
-        self.frequency_minutes.setEnabled(False)
-        self.frequency_seconds.setEnabled(False)
-        self.duration_hours.setEnabled(False)
-        self.duration_minutes.setEnabled(False)
-        self.duration_seconds.setEnabled(False)
+        self.work_hours.setEnabled(False)
+        self.work_minutes.setEnabled(False)
+        self.work_seconds.setEnabled(False)
+        self.break_hours.setEnabled(False)
+        self.break_minutes.setEnabled(False)
+        self.break_seconds.setEnabled(False)
         self.skip_button.setEnabled(True)
         self.stop_button.setEnabled(True)
         self.timer.start(1000)
 
     def skipClock(self):
         if self.phase == 1:
-            self.frequency = (self.frequency_hours.value() * 3600 +
-                              self.frequency_minutes.value() * 60 +
-                              self.frequency_seconds.value())
+            hours, minutes, seconds = self.timeToHMS(self.break_duration)
+            self.time_value.display(f"{hours:02}:{minutes:02}:{seconds:02}")
+            self.work_duration = (self.work_hours.value() * 3600 +
+                                  self.work_minutes.value() * 60 +
+                                  self.work_seconds.value())
             self.phase = 2
             self.skip_button.setText("Skip\nTask")
         elif self.phase == 2:
-            self.duration = (self.duration_hours.value() * 3600 +
-                             self.duration_minutes.value() * 60 +
-                             self.duration_seconds.value())
+            hours, minutes, seconds = self.timeToHMS(self.work_duration)
+            self.time_value.display(f"{hours:02}:{minutes:02}:{seconds:02}")
+            self.break_duration = (self.break_hours.value() * 3600 +
+                                   self.break_minutes.value() * 60 +
+                                   self.break_seconds.value())
             self.phase = 1
             self.skip_button.setText("Skip\nBreak")
 
     def stopClock(self):
         self.timer.stop()
+        hours, minutes, seconds = self.timeToHMS(self.work_duration)
+        self.time_value.display(f"{hours:02}:{minutes:02}:{seconds:02}")
         self.start_button.setEnabled(True)
-        self.frequency_hours.setEnabled(True)
-        self.frequency_minutes.setEnabled(True)
-        self.frequency_seconds.setEnabled(True)
-        self.duration_hours.setEnabled(True)
-        self.duration_minutes.setEnabled(True)
-        self.duration_seconds.setEnabled(True)
+        self.work_hours.setEnabled(True)
+        self.work_minutes.setEnabled(True)
+        self.work_seconds.setEnabled(True)
+        self.break_hours.setEnabled(True)
+        self.break_minutes.setEnabled(True)
+        self.break_seconds.setEnabled(True)
         self.skip_button.setEnabled(False)
         self.stop_button.setEnabled(False)
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key.Key_Escape:
             self.close()
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+            self.position = event.globalPosition().toPoint() - self.frameGeometry().topLeft()
+
+    def mouseMoveEvent(self, event):
+        if self.position is not None:
+            self.move(event.globalPosition().toPoint() - self.position)
+
+    def mouseReleaseEvent(self, event):
+        self.position = None
+
+
+class Settings(QtWidgets.QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.position = None
+        self.setFixedSize(200, 300)
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint, True)
 
     def mousePressEvent(self, event):
         if event.button() == QtCore.Qt.MouseButton.LeftButton:
